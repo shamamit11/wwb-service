09b8e65d58a96cc9ef1708de540b69d594455046
Amit K Sharma <shamamit11@users.noreply.github.com>
2026-06-16 17:49:30 +0200

    Configure Cloudflare R2 disk integration

--- config/filesystems.php
+++ config/filesystems.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'media_disk' => env('MEDIA_DISK', 'media'),
+    'media_disk' => env('MEDIA_DISK', 'r2'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,14 +84,14 @@ return [
 
         'r2' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'key' => env('R2_ACCESS_KEY_ID'),
+            'secret' => env('R2_SECRET_ACCESS_KEY'),
+            'region' => env('R2_REGION', 'auto'),
+            'bucket' => env('R2_BUCKET'),
+            'url' => env('R2_URL'),
+            'endpoint' => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => true,
             'report' => false,
         ],
 
