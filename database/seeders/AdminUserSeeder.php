3fa7d70c3cd88fc66e58d65e8f8e8cc395d35014
Amit K Sharma <shamamit11@users.noreply.github.com>
2026-06-16 23:19:40 +0200

    Add user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public const NAME = 'Admin User';
+    public const EMAIL = 'admin@example.com';
+    public const PASSWORD = 'admin12345';
+
+    public function run(): void
+    {
+        User::query()->updateOrCreate(
+            ['email' => self::EMAIL],
+            [
+                'name' => self::NAME,
+                'password' => self::PASSWORD,
+                'is_admin' => true,
+            ],
+        );
+    }
+}
